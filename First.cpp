cd27903aded28104279599bc9259fc41ef12cd96
prince <princebhagat6301@gmail.com>
2023-08-27 22:20:37 +0530

    Addition Program

---
+++ First.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a , b , sum;
+    cout <<"Enter any number: ";
+    cin >>a>> b;
+    sum = a + b;
+    cout <<"a+b = "<<sum;
+    return(0);
+}
